5f951972e6709c9fc94bb3c4872c56b026b91269
Kirill <kirill2494@mail.ru>
2023-06-22 20:49:34 +0400

    fix bugs after check vercel

--- src/middleware/input-validation-middleware.ts
+++ src/middleware/input-validation-middleware.ts
@@ -6,7 +6,7 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    const reformattedErrors = errors.array().map((err) => {
+    const reformattedErrors = errors.array({ onlyFirstError: true }).map((err) => {
       // @ts-ignore
       return { message: err.msg, field: err.path };
     });
@@ -18,13 +18,7 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
 
 export const isAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const authHeader: string | undefined = req.headers.authorization;
-  if (!authHeader) {
-    res.sendStatus(401);
-    return;
-  }
-
-  const token = authHeader.split(" ")[1];
-  if (!token || token !== "YWRtaW46cXdlcnR5") {
+  if (authHeader !== "Basic YWRtaW46cXdlcnR5") {
     res.sendStatus(401);
     return;
   } else {
